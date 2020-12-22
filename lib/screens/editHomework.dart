3e5522b79e74bdda844e8eb98e97e5274958f302
n2oneProgrammer <dominikwojtasik191@gmail.com>
2020-12-22 22:56:47 +0100

    change position photos with arrows in right up corner in photo object

--- lib/screens/editHomework.dart
+++ lib/screens/editHomework.dart
@@ -7,9 +7,7 @@ class EditHomework extends StatefulWidget {
 
 class _EditHomeworkState extends State<EditHomework> {
   Map data = {};
-  var items = [
-    0,1,2,3,4
-  ];
+  var items = [0, 1, 2, 3, 4];
 
   @override
   Widget build(BuildContext context) {
@@ -20,16 +18,7 @@ class _EditHomeworkState extends State<EditHomework> {
         title: Text(data['title']),
         centerTitle: true,
       ),
-      body: ReorderableListView(
-        onReorder: (oldIndex, newIndex) {
-          setState(() {
-            var a = items[oldIndex];
-            items.removeAt(oldIndex);
-            items.insert(newIndex, a);
-            print(oldIndex.toString() + " " + newIndex.toString());
-            print(items);
-          });
-        },
+      body: ListView(
         children: [
           for (final i in items)
             Dismissible(
@@ -42,11 +31,49 @@ class _EditHomeworkState extends State<EditHomework> {
               child: ListTile(
                   title: Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child: Image.asset(
-                          "assets/testImage/" + (i + 1).toString() + ".jpg"))),
+                      child: generatePage(i))),
             )
         ],
       ),
     );
+  }
+
+  generatePage(var i) {
+    return Stack(children: [
+      Image.asset("assets/testImage/" + (i + 1).toString() + ".jpg"),
+      Align(
+        alignment: Alignment.topRight,
+        child: Column(
+          children: [
+            IconButton(
+              icon: Icon(Icons.arrow_circle_up_rounded),
+              onPressed: () {
+                swapPages(i, true);
+              },
+              iconSize: 50,
+            ),
+            IconButton(
+              icon: Icon(Icons.arrow_circle_down_rounded),
+              onPressed: () {
+                swapPages(i, false);
+              },
+              iconSize: 50,
+            ),
+          ],
+        ),
+      )
+    ]);
+    //     chi )
+  }
+
+  swapPages(int object, bool isUp) {
+    setState(() {
+      int oldIndex = items.indexOf(object);
+      int newIndex = oldIndex + (isUp ? -1 : 1);
+      if (newIndex < 0 || newIndex >= items.length) return;
+      var temp = items[oldIndex];
+      items.removeAt(oldIndex);
+      items.insert(newIndex, temp);
+    });
   }
 }
